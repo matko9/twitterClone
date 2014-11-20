fd2fd84ef988b723743c43b3f3290cd1ec0bc493
Matko Abramovic <matkoabramovic@Matkos-MacBook-Pro.local>
2014-11-20 13:31:21 +0100

    added tests for adding users

--- tests/testsTestUnitTest.php
+++ tests/testsTestUnitTest.php
@@ -2,7 +2,8 @@
 
 namespace Test;
 
-use TwitterClone\Models\Posts;
+use TwitterClone\Models\Posts,
+  TwitterClone\Models\Users;
 
 /**
  * Class UnitTest
@@ -10,7 +11,6 @@ use TwitterClone\Models\Posts;
 class UnitTest extends \UnitTestCase {
   public function test_addPost () {
     $post = new Posts();
-    //if (!Posts::exist('12345678')){
       $post->assign(array(
         'id' => '12345678',
         'users_id' => '12345678',
@@ -18,7 +18,6 @@ class UnitTest extends \UnitTestCase {
       ));
 
       $post->save();
-    //}
 
     $testPost = Posts::findFirstById('12345678');
 
@@ -37,5 +36,42 @@ class UnitTest extends \UnitTestCase {
       false,
       'Deleting post does not work'
     );
+  }
+
+  public function test_addUser () {
+    $user = new Users();
+
+    $user->assign(array(
+      'name' => 'test user name',
+      'surname' => 'test user surname',
+      'nickname' => 'test user nickname',
+      'email' => 'test.user.email@testemail.com',
+      'password' => '$2a$12$57d63765af316acc7045d6ceec705e0b801a6ef905e0b801a6hrd',
+    ));
+
+    $this->assertEquals($user->save(),
+      true,
+      'Adding user does not work'
+    );
+  }
+
+  public function test_addUserWithTheSameEmail () {
+    $user = new Users();
+
+    $user->assign(array(
+      'name' => 'test user name 2',
+      'surname' => 'test user surname 2',
+      'nickname' => 'test user nickname 2',
+      'email' => 'test.user.email@testemail.com',
+      'password' => '$2a$12$57d63765af8319hda8486ceec705e0b801a6ef905e0b801a6hrd',
+    ));
+
+    $this->assertEquals($user->save(),
+      false,
+      'We can add users with the same email address.'
+    );
+
+    $testUser = Users::findFirstByEmail('test.user.email@testemail.com');
+    $testUser->delete();
   }
 }
